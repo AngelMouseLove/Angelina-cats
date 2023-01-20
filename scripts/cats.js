b783a288c63ac937de7e5e4df6d20f672063e2d3
Angelochek <goncharova.angelina9998@gmail.com>
2023-01-20 19:03:47 +0500

    Добавила задание 1 котиков с сервера

---
+++ scripts/cats.js
@@ -0,0 +1,100 @@
+const cats = [
+  {
+    name: "Ося",
+    img_link:
+      "https://i.pinimg.com/736x/4e/77/36/4e7736df9894202fa998e41e8be15f11.jpg",
+    age: 2,
+    rate: 7,
+    favourite: false,
+    description:
+      "Ося – шотландская вислоухая, у нее остеохондродисплазия. Ося спокойная, очень ласковая и контактная. Болезнь не лечится и специального ухода не нужно.",
+    id: 1,
+  },
+  {
+    name: "Базиль",
+    img_link:
+      "https://koshka.top/uploads/posts/2021-12/1638623452_1-koshka-top-p-nyashnikh-kotikov-1.jpg",
+    age: 2,
+    rate: 10,
+    favourite: false,
+    description:
+      "Внимательный, активный и ласковый. Любит играть, катать мяч, и  мурчать на пледе рядом с людьми! Прилично воспитан, приучен к лотку. Вакцинирован, имеет ветеринарный паспорт.",
+    id: 2,
+  },
+  {
+    name: "Риш",
+    img_link:
+      "https://www.friendforpet.ru/api/sites/default/files/2022-01/_DM34706.JPG",
+    age: 1,
+    rate: 10,
+    favourite: true,
+    description:
+      "Риш любит лесенки, канаты. Очень активный и дружелюбный кот. Риш полностью здоров, привит, кастрирован. Использует лоточек и очень аккуратен.",
+    id: 3,
+  },
+  {
+    name: "Соня",
+    img_link:
+      "https://abrakadabra.fun/uploads/posts/2022-01/1641100197_1-abrakadabra-fun-p-milie-kotiki-sokhri-6.jpg",
+    age: 4,
+    rate: 8,
+    favourite: false,
+    description:
+      "Соня обладает мягким и добрым характером. Очень любит всевозможные лакомства и вкусно покушать. Не доверяет людям, потребуется время, чтобы стать ей другом. Приучена к лотку и когтеточке",
+    id: 4,
+  },
+  {
+    name: "Наполеон",
+    img_link: "https://noble-star.com/images/DSC_0021.jpg",
+    age: 0.5,
+    rate: 8,
+    favourite: false,
+    description:
+      "Дымчатый юный котёнок очень любит играть. Полностью доверяет людям, не агрессивный. Ладит с другими животными, приучен к лотку и когтеточке",
+    id: 5,
+  },
+  {
+    name: "Стефани",
+    img_link:
+      "https://www.friendforpet.ru/api/sites/default/files/2022-01/4_30.jpg",
+    age: 6,
+    rate: 9,
+    favourite: false,
+    description:
+      "Прелестная Стефани  трогательная, добродушная и очень-очень общительная девочка как никто другой нуждается в заботе и любви. Приучена к лотку и когтеточке",
+    id: 6,
+  },
+  {
+    name: "Амели",
+    img_link:
+      "https://oir.mobi/uploads/posts/2021-04/1619617508_31-oir_mobi-p-dimchataya-koshka-zhivotnie-krasivo-foto-36.jpg",
+    age: 1,
+    rate: 9,
+    favourite: false,
+    description:
+      "Амели около 1 года с небольшим, здорова, привита, стерилизована. Лоточек и когтеточку знает прекрасно. Очень общительная и нежная, хочет постоянного внимания.",
+    id: 7,
+  },
+  {
+    name: "Рокки",
+    img_link:
+      "https://oir.mobi/uploads/posts/2021-05/1621338493_1-oir_mobi-p-kotyata-porodi-savanna-zhivotnie-krasivo-f-1.jpg",
+    age: 1,
+    rate: 10,
+    favourite: false,
+    description:
+      "Очаровательный активный кот Рокки, порода Саванна, находится в постоянном движении! Очаровательный и ласковый кот. Приучен к лотку, ладит с другими котами, привит.",
+    id: 8,
+  },
+  {
+    name: "Бегемот",
+    img_link:
+      "https://damion.club/uploads/posts/2022-02/1644042759_10-damion-club-p-persidskii-kot-chernii-zhivotnie-10.jpg",
+    age: 1,
+    rate: 9,
+    favourite: true,
+    description:
+      "Немного боязливый, но очень добрый и нежный кот. Приучен к лотку и когтеточке, ладит с детьми, привит. Станет вам хорошим другом",
+    id: 9,
+  },
+];
